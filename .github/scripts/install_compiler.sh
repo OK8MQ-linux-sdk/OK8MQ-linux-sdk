727cd4c51f2312c41f09aeedf563fbbf4f5c311b
comet959 <1376723538@qq.com>
2023-12-11 22:19:27 +0800

    CICD

--- .github/scripts/install_compiler.sh
+++ .github/scripts/install_compiler.sh
@@ -2,7 +2,7 @@
 
 pwd
 # 启动要执行的命令，这里假设是执行 ./install_compiler.sh
-spawn ../../temp/╜╗▓ц▒р╥ы╣д╛▀/fsl-imx-xwayland-glibc-x86_64-meta-toolchain-qt5-aarch64-toolchain-5.4-zeus.sh
+spawn ./╜╗▓ц▒р╥ы╣д╛▀/fsl-imx-xwayland-glibc-x86_64-meta-toolchain-qt5-aarch64-toolchain-5.4-zeus.sh
 
 # 匹配脚本输出中的关键字，通常是要求输入的提示信息
 expect "Enter target directory for SDK (default: /opt/fsl-imx-xwayland/5.4-zeus):"
